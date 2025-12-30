3a0ef7cf5cd2d25bec2ea8355a3ae62de9151fae
BlanchardIlela <blancilela@gmail.com>
2025-12-30 05:17:07 +0100

    Deuxième methode

--- functions/math.js
+++ functions/math.js
@@ -1,3 +1,12 @@
 export function clamp(value, min, max) {
+
+    /* if (value > max) {
+        return max
+    } else if (value < min) {
+        return min
+    }
+    return value */
+
+    return Math.min(max, Math.max(min, value))
     
 }
